d548ff641b215533d050dd6a0b22b2e4db32358d
mokashi21 <101655816+mokashi21@users.noreply.github.com>
2022-08-04 21:39:44 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,16 @@
+let user = {
+    name : "India",
+    capital : "Delhi",
+    financialcapital :"mumbai",
+    moneymode : "rupee",
+    time : "GMT530",
+
+}
+
+let {name : i,capital : c,financialcapital : f,moneymode : m,time: t} = user;
+
+console.log(i)
+console.log(c)
+console.log(f)
+console.log(m)
+console.log(t)
